9daf3f6af6a1952ad629a13f850d92457b9d50b9
4d46 <lvhe29@outlook.com>
2023-02-07 00:06:22 +0800

    feat: 优化title随机逻辑

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 function random(seed) {
   if (!seed) return Math.random();
   const numStr = '0.' + Math.sin(seed).toString().slice(6);
@@ -16,7 +15,7 @@ function randomMoveCards(cards) {
     const rangeX = 4;
     const rangeY = 1;
     const randomX = rangeX * (random(seed) * 2 - 1);
-    const randomY = rangeY * (random(seed && (seed + 0.1)) * 2 - 1);
+    const randomY = rangeY * (random(seed && seed + 0.1) * 2 - 1);
     move(e, randomX.toFixed(3), randomY.toFixed(3));
   });
 }
@@ -24,28 +23,36 @@ function randomMoveCards(cards) {
 function randomMoveTitle(title) {
   title.forEach((e, i) => {
     var moveY = random();
-    var top = 'calc(' + moveY.toFixed(2) + ' * (100% - ' + e.clientHeight + 'px))';
-    e.style.paddingTop = top;
+    var top =
+      'calc(' + moveY.toFixed(2) + ' * (100% - ' + e.clientHeight + 'px))';
+    // e.style.paddingTop = top;
+    e.style.marginTop = top;
   });
 }
 
 function onload() {
   // const cards = document.querySelectorAll('.card');
   // randomMoveCards(cards);
-  var title = document.querySelectorAll("#card-title > div")
-  randomMoveTitle(title)
+  var title = document.querySelectorAll('#card-title > div');
+  randomMoveTitle(title);
+  randomTitleWidthTimer();
 }
 
-var resizeTimer = null;
-function onResize() {
-  if (resizeTimer !== null) return;
-  var title = document.querySelectorAll("#card-title > div")
-  randomMoveTitle(title)
-  resizeTimer = setTimeout(function () {
-    clearTimeout(resizeTimer);
-    resizeTimer = null;
+var randomTitleTimer = null;
+var randomTitleLoopTimer = null;
+function randomTitleWidthTimer() {
+  if (randomTitleTimer !== null) return;
+  var title = document.querySelectorAll('#card-title > div');
+  randomTitleTimer = setTimeout(function () {
+    clearTimeout(randomTitleTimer);
+    randomTitleTimer = null;
   }, 300);
+  randomMoveTitle(title);
+  clearTimeout(randomTitleLoopTimer);
+  randomTitleLoopTimer = setTimeout(function () {
+    randomTitleWidthTimer();
+  }, 5000);
 }
 
 document.addEventListener('DOMContentLoaded', onload);
-window.onresize = onResize;
+window.onresize = randomTitleWidthTimer;
